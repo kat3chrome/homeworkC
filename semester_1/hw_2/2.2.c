621ea242b7b398548751053abbd40bfe8c2c9034
kate <shandkat3@gmail.com>
2019-10-20 01:48:49 +0300

    ready

---
+++ semester_1/hw_2/2.2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void printFareiSequence(int value, int leftFractionNumerator, int leftFractionDenominator, int rightFractionNumerator, int rightFractionDenominator)
+{
+  int newNumerator = leftFractionNumerator + rightFractionNumerator;
+  int newDenominator = leftFractionDenominator + rightFractionDenominator;
+  if (newDenominator <= value)
+  {
+    printFareiSequence(value, leftFractionNumerator, leftFractionDenominator, newNumerator, newDenominator);
+    printf("%d/%d ", newNumerator, newDenominator);
+    printFareiSequence(value, newNumerator, newDenominator, rightFractionNumerator, rightFractionDenominator);
+  }
+}
+int main()
+{
+  int value = 0;
+  printf("Enter maximum denominator : ");
+  scanf("%d", &value);
+  printFareiSequence(value, 0, 1, 1, 1);
+  printf("\n");
+  return 0;
+}
