6aea1b0e322b2958c72341ae83dcc353454ade3f
kate <shandkat3@gmail.com>
2019-10-21 10:28:07 +0300

    ready

---
+++ semester_1/hw_3/3.1.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int swap(int array[], int relocatable1Index, int relocatable2Index)
+{
+  int temp = array[relocatable1Index];
+  array[relocatable1Index] = array[relocatable2Index];
+  array[relocatable2Index] = temp;
+}
+
+void siftDown(int array[], int sizeArray, int index)
+{
+  int indexRightChild = index * 2 + 1;
+  int indexLeftChild = index * 2 + 2;
+  int indexMinimum = index;
+
+  if ((indexLeftChild < sizeArray) && (array[indexLeftChild] < array[indexMinimum]))
+  {
+    indexMinimum = indexLeftChild;
+  }
+  if ((indexRightChild < sizeArray) && (array[indexRightChild] < array[indexMinimum]))
+  {
+    indexMinimum = indexRightChild;
+  }
+  if (indexMinimum != index)
+  {
+    swap(array, indexMinimum, index);
+    siftDown(array, sizeArray, indexMinimum);
+  }
+}
+
+void createHeap(int array[], int sizeArray)
+{
+  for (int i = sizeArray / 2 - 1; i >= 0; i--)
+  {
+    siftDown(array, sizeArray, i);
+  }
+}
+
+void heapSort(int array[], int sizeArray)
+{
+  createHeap(array, sizeArray);
+
+  for (int i = sizeArray - 1; i > 0; i--)
+  {
+    swap(array, 0, i);
+    siftDown(array, i, 0);
+  }
+}
+
+int main()
+{
+  int sizeArray = 0;
+  printf("Enter size of array : ");
+  scanf("%d", &sizeArray);
+  int *array = malloc(sizeArray * sizeof(int));
+  
+  printf("Enter array : ");
+  for (int i = 0; i < sizeArray; i++)
+  {
+    scanf("%d", &array[i]);
+  }
+
+  heapSort(array, sizeArray);
+
+  printf("Sorted array : ");
+  for (int i = 0; i < sizeArray; i++)
+  {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
+
+
+  free(array);
+  return 0;
+}
