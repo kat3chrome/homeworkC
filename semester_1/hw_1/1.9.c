bc94ad005c3002c6c7bb793d9528ef91e40485d2
kate <shandkat3@gmail.com>
2019-10-30 14:20:57 +0300

    fix

--- semester_1/hw_1/1.9.c
+++ semester_1/hw_1/1.9.c
@@ -2,23 +2,23 @@
 
 long long binaryPower(int basis, int power)
 {
-      if (power == 0)
-      {
-          return 1;
-      }
-
-      if (power % 2 == 0)
-      {
-          long long result = binaryPower(basis, power / 2);
-          return result * result;
-      }
-
-      return binaryPower(basis, power - 1) * basis;
+  if (power == 0)
+  {
+    return 1;
   }
+
+  if (power % 2 == 0)
+  {
+    long long result = binaryPower(basis, power / 2);
+    return result * result;
+  }
+
+  return binaryPower(basis, power - 1) * basis;
+}
 int main()
 {
-  int basis = 1;
-  int power = 1;
+  int basis = 0;
+  int power = 0;
 
   printf("%s", "Enter basis(a) and power(n) : ");
   scanf("%d%d", &basis, &power);
