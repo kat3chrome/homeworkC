9a6bd67b7731634ebe75003a44dfee6eaa59186c
kate <shandkat3@gmail.com>
2019-11-07 00:43:09 +0300

    in process

--- semester_1/hw_4/4.2.c
+++ semester_1/hw_4/4.2.c
@@ -29,7 +29,7 @@ bool existsFile(char fileAdress[100])
 char getRandomCharacter()
 {
   srand(time(0));
-  return (char)(rand()%20+ 65);
+  return (char)(rand() % 20 + 65);
 }
 
 char* concatenateTwoStrings(const char *string1, const char *string2)
@@ -60,7 +60,7 @@ void getNameOfTemporaryFile(int lengthTemporaryFileAdress, char temporaryFileAdr
 
 void printCommands()
 {
-  char commands[] = "0 - exit\n1 - add an entry (name and phone number)\n2 - find phone by name\n3 - find the name on the phone\n4 - save current data to file\n5 - print a list of commands";
+  char commands[] = "0 - exit\n1 - add an entry (name and phone number)\n2 - find phone by name\n3 - find the name by phone\n4 - save current data to file\n5 - print a list of commands";
   printf("List of commands :\n%s\n", commands);
 }
 
@@ -71,22 +71,7 @@ void printGreetings()
   printf("! Thank you for choosing our software !\n");
 }
 
-void createPhoneBookFileOrDoNothing()
-{
-  char nameOfPhoneBook[] = "PHONE.BOOK";
-  FILE *phoneBook;
-  if (existsFile(nameOfPhoneBook))
-  {
-    phoneBook = fopen(nameOfPhoneBook, "r");
-    fclose(phoneBook);
-    return;
-  }
-
-  phoneBook = fopen(nameOfPhoneBook, "w");
-  fclose(phoneBook);
-}
-
-void addContactToTemporayFile(FILE *temporaryFile)
+void addContactToTemporayFile(char temporaryFileAdress[])
 {
   char number[100];
   char name[100];
@@ -95,15 +80,77 @@ void addContactToTemporayFile(FILE *temporaryFile)
 
   printf("Enter name and phone : ");
   scanf("%s%s", name, number);
-  fprintf(temporaryFile, "`%s:%s`", name, number);
+
+  while (strstr(name, "¡") != NULL || strstr(number, "¡") != NULL)
+  {
+    printf("¡Enter the correct data!\nDon't use \"¡\" ,please\nEnter name and phone : ");
+    scanf("%s%s", name, number);
+  }
+
+  FILE *temporaryFile = fopen(temporaryFileAdress, "a+");
+  fprintf(temporaryFile, "¡%s¡¡¡%s¡", name, number);
+  fclose(temporaryFile);
 }
 
-saveTheCurrentDatatoFile(FILE *phoneBook, FILE *temporaryFile)
+void saveTheCurrentDataToFile(char phoneBookAdress[], char temporaryFileAdress[])
 {
+  FILE *phoneBook = fopen(phoneBookAdress, "a+");
+  FILE *temporaryFile = fopen(temporaryFileAdress, "a+");
+  char temporaryCharaster;
+  int index = 0;
 
+  while ((temporaryCharaster = getc(temporaryFile)) != EOF)
+  {
+    fprintf(phoneBook, "%c", temporaryCharaster);
+  }
+
+  fclose(phoneBook);
+  fclose(temporaryFile);
+  remove(temporaryFileAdress);
 }
 
-void commandProcessing(FILE *phoneBook, FILE *temporaryFile)
+const char* findSubstringInArrayOfNamesAndNumbers(char phoneBookAdress[], char searchString[], int position)
+{
+  // static char searchString1[] = "aaa";
+  // return searchString1;
+  if (position == 1)
+  {
+    static char number[] = "88005553535";
+    return number;
+  }
+  else
+  {
+    static char name[] = "Kat3";
+    return name;
+  }
+}
+
+void findPhoneNumberByNameAndPrintResult(char phoneBookAdress[])
+{
+  char name[100];
+  stringInitialization(name);
+
+  printf("Enter enter the name to search : ");
+  scanf("%s", name);
+
+  const char* number = findSubstringInArrayOfNamesAndNumbers(phoneBookAdress, name, 1);
+
+  printf("%s has this number : %s\n", name, number);
+}
+
+void findNameByPhoneNumberAndPrintResult(char phoneBookAdress[])
+{
+  char number[100];
+  stringInitialization(number);
+
+  printf("Enter enter the number to search : ");
+  scanf("%s", number);
+
+  const char* name = findSubstringInArrayOfNamesAndNumbers(phoneBookAdress, number, 2);
+  printf("%s has this number : %s\n", name, number);
+}
+
+void commandProcessing(char phoneBookAdress[], char temporaryFileAdress[])
 {
   int command = 0;
   printCommands();
@@ -119,17 +166,19 @@ void commandProcessing(FILE *phoneBook, FILE *temporaryFile)
         break;
 
       case 1:
-        addContactToTemporayFile(temporaryFile);
+        addContactToTemporayFile(temporaryFileAdress);
         break;
 
       case 2:
+        findPhoneNumberByNameAndPrintResult(phoneBookAdress);
         break;
 
       case 3:
+        findNameByPhoneNumberAndPrintResult(phoneBookAdress);
         break;
 
       case 4:
-        saveTheCurrentDatatoFile(phoneBook, temporaryFile);
+        saveTheCurrentDataToFile(phoneBookAdress, temporaryFileAdress);
         break;
 
       case 5:
@@ -146,23 +195,22 @@ void commandProcessing(FILE *phoneBook, FILE *temporaryFile)
 
 int main()
 {
-  // const int lengthTemporaryFileAdress = 10;
-  // char temporaryFileAdress[lengthTemporaryFileAdress];
-  // stringInitialization(temporaryFileAdress);
-  // getNameOfTemporaryFile(lengthTemporaryFileAdress, temporaryFileAdress);
-  char temporaryFileAdress[7] = "T.temp";
-  createPhoneBookFileOrDoNothing();
+  const int lengthTemporaryFileAdress = 10;
+  char temporaryFileAdress[lengthTemporaryFileAdress];
+  stringInitialization(temporaryFileAdress);
+  getNameOfTemporaryFile(lengthTemporaryFileAdress, temporaryFileAdress);
+  char phoneBookAdress[] = "PHONE.BOOK";
 
   printGreetings();
+  FILE *phoneBook = fopen(phoneBookAdress, "w");
+  FILE *temporaryFile = fopen(temporaryFileAdress, "w");
+  fclose(phoneBook);
+  fclose(temporaryFile);
 
-  FILE *phoneBook;
-  phoneBook = fopen(temporaryFileAdress, "r");
-  FILE *temporaryFile;
-  temporaryFile = fopen(temporaryFileAdress, "w");
-
-  commandProcessing(phoneBook, temporaryFile);
+  commandProcessing(phoneBookAdress, temporaryFileAdress);
 
   fclose(phoneBook);
   fclose(temporaryFile);
+  remove(temporaryFileAdress);
   return 0;
 }
