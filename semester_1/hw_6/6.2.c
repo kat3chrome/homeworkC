da5905ad7f06f14624077aa997090c72cfd754c0
kate <shandkat3@gmail.com>
2019-11-07 03:18:16 +0300

    ~

--- semester_1/hw_6/6.2.c
+++ semester_1/hw_6/6.2.c
@@ -3,7 +3,7 @@
 
 void decimalToBinary(int decimalNumber, int *binaryNumber ,int numberOfBinaryDigits)
 {
-  int index = 0;
+  int index = 1;
   while (decimalNumber > 0)
   {
     binaryNumber[index] = decimalNumber % 2;
@@ -40,16 +40,54 @@ void printBinaryNumber(int *binaryNumber, int numberOfBinaryDigits)
   printf("\n");
 }
 
-// void sumOfBinaryNumbers(int *binaryNumber1, int *binaryNumber2, int *binarySum, int numberOfBinaryDigits)
-// {
-//
-// }
+void sumOfBinaryNumbers(int *binaryNumber1, int *binaryNumber2, int *binarySum, int numberOfBinaryDigits)
+{
+  int buckupDigit = 0;
+  for (int i = numberOfBinaryDigits - 1; i >= 1; i--)
+  {
+    int digitFromBinaryNumber1 = binaryNumber1[i];
+    int digitFromBinaryNumber2 = binaryNumber2[i];
+
+    int sumOfCurrentGigits = digitFromBinaryNumber1 + digitFromBinaryNumber2;
+
+    switch (sumOfCurrentGigits)
+    {
+      case 0:
+        binarySum[i] = buckupDigit;
+        buckupDigit = 0;
+        break;
+      case 1:
+        if (buckupDigit == 0)
+        {
+          binarySum[i] = 1;
+        }
+        else
+        {
+          binarySum[i] = 0;
+          buckupDigit = 0;
+        }
+        break;
+      case 2:
+        if (buckupDigit == 0)
+        {
+          binarySum[i] = 0;
+          buckupDigit = 1;
+        }
+        else
+        {
+          binarySum[i] = 1;
+          buckupDigit = 1;
+        }
+        break;
+    }
+  }
+}
 
 int main()
 {
   int decimalNumber1 = 0;
   int decimalNumber2 = 0;
-  int numberOfBinaryDigits = 32;
+  int numberOfBinaryDigits = 33;
   int binaryNumber1[numberOfBinaryDigits];
   int binaryNumber2[numberOfBinaryDigits];
   initializingOfArray(binaryNumber1, numberOfBinaryDigits);
@@ -64,14 +102,19 @@ int main()
   binaryToDecimal(&decimalNumber1, binaryNumber1, numberOfBinaryDigits);
   binaryToDecimal(&decimalNumber2, binaryNumber2, numberOfBinaryDigits);
 
-  // binarySum[numberOfBinaryDigits];
-  // initializingOfArray(binarySum, numberOfBinaryDigits);
-  // sumOfBinaryNumbers(binaryNumber1,  binaryNumber2, binarySum, numberOfBinaryDigits);
+  int binarySum[numberOfBinaryDigits];
+  initializingOfArray(binarySum, numberOfBinaryDigits);
+  sumOfBinaryNumbers(binaryNumber1,  binaryNumber2, binarySum, numberOfBinaryDigits);
 
   printf("First number in binary : ");
   printBinaryNumber(binaryNumber1, numberOfBinaryDigits);
   printf("Second number in binary : ");
   printBinaryNumber(binaryNumber2, numberOfBinaryDigits);
+  printf("Sum of number equal : ");
+  printBinaryNumber(binarySum, numberOfBinaryDigits);
 
+  int decimalSum = 0;
+  binaryToDecimal(&decimalSum, binarySum, numberOfBinaryDigits);
+  printf("Decimal sum %d\n", decimalSum);
   return 0;
 }
