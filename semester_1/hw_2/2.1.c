a20efa83a0061bc166da72e44e0b4a8a9217d71a
kate <shandkat3@gmail.com>
2019-11-08 20:40:49 +0300

    fix

--- semester_1/hw_2/2.1.c
+++ semester_1/hw_2/2.1.c
@@ -3,50 +3,50 @@
 
 int min(int numberA, int numberB)
 {
-    return (numberA < numberB ? numberA : numberB);
+  return (numberA < numberB ? numberA : numberB);
 }
 
-void printSums(int needAdd, int *sum, int numberOfSum)
+void printSums(int needAdd, int *currentSummands, int numberOfSummands)
 {
   if (needAdd == 0)
+  {
+    for (int i = 0; i < numberOfSummands; i++)
     {
-      for (int i = 0; i < numberOfSum; i++)
-      {
-        printf("%d", sum[i]);
-        printf("%s", (i + 1 == numberOfSum ? ";\n" : " + "));
-      }
-
-      return;
+      printf("%d",currentSummands[i]);
+      printf("%s", (i + 1 == numberOfSummands ? ";\n" : " + "));
     }
+
+    return;
+  }
 
   int maxTerm = 0;
 
-  if (numberOfSum > 0)
-    {
-      maxTerm = min(needAdd, sum[numberOfSum - 1]);
-    }
+  if (numberOfSummands > 0)
+  {
+    maxTerm = min(needAdd,currentSummands[numberOfSummands - 1]);
+  }
   else
-    {
-      maxTerm = needAdd;
-    }
+  {
+    maxTerm = needAdd;
+  }
 
   for (int term = maxTerm; term > 0; term--)
   {
-    sum[numberOfSum] = term;
-    printSums(needAdd - term, sum, numberOfSum + 1);
+   currentSummands[numberOfSummands] = term;
+    printSums(needAdd - term,currentSummands, numberOfSummands + 1);
   }
 }
 
 int main()
 {
-    int number = 0;
-    printf("Enter the number:");
-    scanf("%d", &number);
+  int number = 0;
+  printf("Enter the number:");
+  scanf("%d", &number);
 
-    printf("The sums of %d are:\n", number);
-    int *numberSum = malloc(number * sizeof(int));
-    printSums(number, numberSum, 0);
+  printf("ThecurrentSummandss of %d are:\n", number);
+  int *numberSum = malloc(number * sizeof(int));
+  printSums(number, numberSum, 0);
 
-    free(numberSum);
-    return 0;
+  free(numberSum);
+  return 0;
 }
