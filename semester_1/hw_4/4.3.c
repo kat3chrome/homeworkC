d2941c2f53b074a538a5f8d67dc16e474ad6f272
kate <shandkat3@gmail.com>
2019-11-25 22:57:48 +0300

    fix

--- semester_1/hw_4/4.3.c
+++ semester_1/hw_4/4.3.c
@@ -1,89 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printBySpiral(int **array, int number)
+void printLine(int signOne, int *coordinate1, int *coordinate2, int arrayRowSize, int **array ,int currentIndex)
 {
-  int coordinate1 = (number / 2) + 1;
-  int coordinate2 = (number / 2) + 1;
+  for (int j = 1; j <= currentIndex; j++)
+  {
+    if (*coordinate1 == arrayRowSize && *coordinate2 == arrayRowSize)
+    {
+      break;
+    }
+    *coordinate2 += signOne;
+    printf("%d ", array[*coordinate1][*coordinate2]);
+  }
+  for (int j = 1; j <= currentIndex; j++)
+  {
+    if (*coordinate1 == arrayRowSize && *coordinate2 == arrayRowSize)
+    {
+      break;
+    }
+    *coordinate1 -= signOne;
+    printf("%d ", array[*coordinate1][*coordinate2]);
+  }
+}
+
+void printBySpiral(int **array, int arrayRowSize)
+{
+  int coordinate1 = (arrayRowSize / 2) + 1;
+  int coordinate2 = (arrayRowSize / 2) + 1;
   printf("%d ", array[coordinate1][coordinate2]);
-  for (int i = 1; i <= number; i++)
+  for (int i = 1; i <= arrayRowSize; i++)
   {
     if ((i % 2) != 0)
     {
-      for (int j = 1; j <= i; j++)
-      {
-        if (coordinate1 == number && coordinate2 == number)
-        {
-          break;
-        }
-        coordinate2++;
-        printf("%d ", array[coordinate1][coordinate2]);
-      }
-      for (int j = 1; j <= i; j++)
-      {
-        if (coordinate1 == number && coordinate2 == number)
-        {
-          break;
-        }
-        coordinate1--;
-        printf("%d ", array[coordinate1][coordinate2]);
-      }
+      printLine(1, &coordinate1, &coordinate2, arrayRowSize, array, i);
     }
     if ((i % 2) == 0)
     {
-      for (int j = 1; j <= i; j++)
-      {
-        if (coordinate1 == number && coordinate2 == number)
-        {
-          break;
-        }
-        coordinate2--;
-        printf("%d ", array[coordinate1][coordinate2]);
-      }
-      for (int j = 1; j <= i; j++)
-      {
-        if (coordinate1 == number && coordinate2 == number)
-        {
-          break;
-        }
-        coordinate1++;
-        printf("%d ", array[coordinate1][coordinate2]);
-      }
+      printLine(-1, &coordinate1, &coordinate2, arrayRowSize, array, i);
     }
   }
 }
 
 int main()
 {
-  printf("Enter the number of elements per side of the array : ");
-  int number = 0;
-  scanf("%d", &number);
-  while (number % 2 == 0)
+  printf("Enter the arrayRowSize of elements per side of the array : ");
+  int arrayRowSize = 0;
+  scanf("%d", &arrayRowSize);
+  while (arrayRowSize % 2 == 0)
   {
-    printf("The number must be odd, enter the number again : ");
-    scanf("%d\n", &number);
+    printf("The arrayRowSize must be odd, enter the arrayRowSize again : ");
+    scanf("%d", &arrayRowSize);
   }
 
-  int **array = (int**)malloc(sizeof(int*) * (number + 1));
-  for (int i = 1; i <= number; i++)
+  int **array = (int**)malloc(sizeof(int*) * (arrayRowSize + 1));
+  for (int i = 1; i <= arrayRowSize; i++)
   {
-      array[i] = (int*)malloc(sizeof(int) * (number + 1));
+      array[i] = (int*)malloc(sizeof(int) * (arrayRowSize + 1));
   }
 
   printf("Enter the array line by line\n");
-  for (int i = 1; i <= number; i++)
+  for (int i = 1; i <= arrayRowSize; i++)
   {
     printf("Enter %d line : ", i);
-    for (int j = 1; j <= number; j++)
+    for (int j = 1; j <= arrayRowSize; j++)
     {
       scanf("%d", &array[i][j]);
     }
   }
 
   printf("Output elements in a spiral : ");
-  printBySpiral(array, number);
+  printBySpiral(array, arrayRowSize);
 
-  for (int i = 1; i <= number; i++)
+  for (int i = 1; i <= arrayRowSize; i++)
   {
     free(array[i]);
   }
