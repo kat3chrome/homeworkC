79969e17f0d8a603dbac7129ec175859c6b40f98
kate <shandkat3@gmail.com>
2019-12-03 18:11:58 +0300

    поправил отступы, поменял условия считывания размера массивы

--- semester_1/hw_1/1.11.c
+++ semester_1/hw_1/1.11.c
@@ -3,80 +3,80 @@
 
 void swap(int *first, int *second)
 {
-    int temp = *first;
-    *first = *second;
-    *second = temp;
+  int temp = *first;
+  *first = *second;
+  *second = temp;
 }
 
 int quickSort(int array[], int startOfArray, int finishOfArray)
 {
-    if (startOfArray >= finishOfArray)
+  if (startOfArray >= finishOfArray)
+  {
+    return 0;
+  }
+
+  int referenceElement = array[(startOfArray + finishOfArray) / 2];
+  int leftIndex = startOfArray;
+  int rightIndex = finishOfArray;
+
+  do
+  {
+    while (array[leftIndex] < referenceElement)
     {
-        return 0;
+      leftIndex++;
     }
 
-    int referenceElement = array[(startOfArray + finishOfArray) / 2];
-    int leftIndex = startOfArray;
-    int rightIndex = finishOfArray;
-
-    do
+    while (array[rightIndex] > referenceElement)
     {
-        while (array[leftIndex] < referenceElement)
-        {
-            leftIndex++;
-        }
-
-        while (array[rightIndex] > referenceElement)
-        {
-            rightIndex--;
-        }
-
-        if (leftIndex <= rightIndex)
-        {
-            swap(&array[leftIndex], &array[rightIndex]);
-            leftIndex++;
-            rightIndex--;
-        }
+      rightIndex--;
     }
-    while (leftIndex <= rightIndex);
 
-    quickSort(array, startOfArray, rightIndex);
-    quickSort(array, leftIndex, finishOfArray);
+    if (leftIndex <= rightIndex)
+    {
+      swap(&array[leftIndex], &array[rightIndex]);
+      leftIndex++;
+      rightIndex--;
+    }
+  }
+  while (leftIndex <= rightIndex);
+
+  quickSort(array, startOfArray, rightIndex);
+  quickSort(array, leftIndex, finishOfArray);
 }
 
 int main()
 {
-    const int maximumArraySize = 100;
-    int array[maximumArraySize];
-    for (int i = 0; i < maximumArraySize; i++)
-    {
-        array[i] = 0;
-    }
+  const int maximumArraySize = 100;
+  int array[maximumArraySize];
+  for (int i = 0; i < maximumArraySize; i++)
+  {
+    array[i] = 0;
+  }
 
-    int arraySize = 0;
-    printf("Enter size of array : ");
+  int arraySize = 0;
+  printf("Enter size of array : ");
+  scanf("%d", &arraySize);
+
+  while (arraySize <= 0 || arraySize > 100)
+  {
+    printf("Enter correct size of array ([1:100]) : ");
     scanf("%d", &arraySize);
+  }
 
-    while (arraySize <= 0 || arraySize > 100)
-    {
-      printf("Enter correct size of array ([1:100]) : ");
-      scanf("%d", &arraySize);
-    }
+  printf("Enter elements of array : ");
+  for (int i = 0; i < arraySize; i++)
+  {
+    scanf("%d", &array[i]);
+  }
 
-    printf("Enter elements of array : ");
-    for (int i = 0; i < arraySize; i++)
-    {
-        scanf("%d", &array[i]);
-    }
+  quickSort(array, 0, arraySize - 1);
 
-    quickSort(array, 0, arraySize - 1);
+  printf("Sorted array: ");
+  for (int i = 0; i < arraySize; i++)
+  {
+    printf("%d ", array[i]);
+  }
+  printf("\n");
 
-    printf("Sorted array: ");
-    for (int i = 0; i < arraySize; i++)
-    {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-
-    return 0;
+  return 0;
 }
