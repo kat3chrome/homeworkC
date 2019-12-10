e8aceb418129bce89585a296415512c7281bc523
kate <shandkat3@gmail.com>
2019-12-10 16:26:29 +0300

    не работает сложение отрицательного и положительного

--- semester_1/hw_6/6.2.c
+++ semester_1/hw_6/6.2.c
@@ -2,6 +2,8 @@
 #include <stdbool.h>
 #include <math.h>
 
+enum operands {MINUS = -1, PLUS = 1};
+
 void arrayReverse(int *array, int sizeOfArray)
 {
 
@@ -87,10 +89,13 @@ void sumOfBinaryNumbers(int *binaryNumber1, int *binaryNumber2, int *binarySum, 
     switch (sumOfCurrentDigits)
     {
       case 0:
+      {
         binarySum[i] = buckupDigit;
         buckupDigit = 0;
         break;
+      }
       case 1:
+      {
         if (buckupDigit == 0)
         {
           binarySum[i] = 1;
@@ -101,7 +106,9 @@ void sumOfBinaryNumbers(int *binaryNumber1, int *binaryNumber2, int *binarySum, 
           buckupDigit = 1;
         }
         break;
+      }
       case 2:
+      {
         if (buckupDigit == 0)
         {
           binarySum[i] = 0;
@@ -113,7 +120,89 @@ void sumOfBinaryNumbers(int *binaryNumber1, int *binaryNumber2, int *binarySum, 
           buckupDigit = 1;
         }
         break;
+      }
     }
+  }
+}
+
+void copyOfBinaryNumber(int *recipientBinaryNumber, int *senderBinaryNumber, int numberOfBinaryDigits)
+{
+  for (int i = 0; i < numberOfBinaryDigits; i++)
+  {
+    recipientBinaryNumber[i] = senderBinaryNumber[i];
+  }
+}
+
+void binaryNumberToReverseCode(int *temporaryBinaryNumber, int *binaryNumber, int numberOfBinaryDigits)
+{
+  for (int i = 0; i < numberOfBinaryDigits; i++)
+  {
+    if (binaryNumber[i] == 0)
+    {
+      temporaryBinaryNumber[i] = 1;
+    }
+    else
+    {
+      temporaryBinaryNumber[i] = 0;
+    }
+  }
+}
+
+void binaryNumberToAdditionalCode(int *temporaryBinaryNumber, int *binaryNumber, int numberOfBinaryDigits)
+{
+  binaryNumberToReverseCode(temporaryBinaryNumber, binaryNumber, numberOfBinaryDigits);
+  int binaryOne[numberOfBinaryDigits];
+  initializingOfArray(binaryOne, numberOfBinaryDigits);
+  binaryOne[numberOfBinaryDigits - 1] = 1;
+
+  sumOfBinaryNumbers(temporaryBinaryNumber, binaryOne, temporaryBinaryNumber, numberOfBinaryDigits);
+  printBinaryNumber(temporaryBinaryNumber, numberOfBinaryDigits);
+}
+
+void differenceOfBinaryNumbers(int *binaryNumber1, int *binaryNumber2, int *binaryDifference, int numberOfBinaryDigits)
+{
+  int temporaryBinaryNumber[numberOfBinaryDigits];
+  initializingOfArray(temporaryBinaryNumber, numberOfBinaryDigits);
+  binaryNumberToAdditionalCode(temporaryBinaryNumber, binaryNumber2, numberOfBinaryDigits);
+
+  sumOfBinaryNumbers(binaryNumber1, temporaryBinaryNumber, binaryDifference, numberOfBinaryDigits);
+  printBinaryNumber(binaryDifference, numberOfBinaryDigits);
+
+
+}
+
+int getSignOfDecimalNumber(int number)
+{
+  return number > 0 ? PLUS : MINUS;
+}
+
+void makeUnsignedDecimalNumber(int *number)
+{
+  if (*number < 0)
+  {
+    *number = -*number;
+  }
+}
+
+void printSignOfNumber(int sign)
+{
+  if (sign == PLUS)
+  {
+    printf("+");
+  }
+  else
+  {
+    printf("-");
+  }
+}
+
+void smallestNumberOnFirstPlace(int *decimalNumber1, int *decimalNumber2)
+{
+  if (*decimalNumber1 > *decimalNumber2)
+  {
+    int temporary = *decimalNumber1;
+    *decimalNumber1 = *decimalNumber2;
+    *decimalNumber2 = temporary;
   }
 }
 
@@ -130,25 +219,47 @@ int main()
   printf("Enter the numbers : ");
   scanf("%d%d", &decimalNumber1, &decimalNumber2);
 
+  int signOfFirstNumber = getSignOfDecimalNumber(decimalNumber1);
+  int signOfSecondNumber = getSignOfDecimalNumber(decimalNumber2);
+  int signOfSumOfNumbers = getSignOfDecimalNumber(decimalNumber1 + decimalNumber2);
+  int signOfCompositionOfNumbers = getSignOfDecimalNumber(decimalNumber1 * decimalNumber2);
+
+  makeUnsignedDecimalNumber(&decimalNumber1);
+  makeUnsignedDecimalNumber(&decimalNumber2);
+  smallestNumberOnFirstPlace(&decimalNumber1, &decimalNumber2);
+
   decimalToBinary(decimalNumber1, binaryNumber1, numberOfBinaryDigits);
   decimalToBinary(decimalNumber2, binaryNumber2, numberOfBinaryDigits);
+
+  int binarySum[numberOfBinaryDigits];
+  initializingOfArray(binarySum, numberOfBinaryDigits);
+
+  if (signOfCompositionOfNumbers == MINUS)
+  {
+    differenceOfBinaryNumbers(binaryNumber1, binaryNumber2, binarySum, numberOfBinaryDigits);
+  }
+  else
+  {
+    sumOfBinaryNumbers(binaryNumber1,  binaryNumber2, binarySum, numberOfBinaryDigits);
+  }
 
   binaryToDecimal(&decimalNumber1, binaryNumber1, numberOfBinaryDigits);
   binaryToDecimal(&decimalNumber2, binaryNumber2, numberOfBinaryDigits);
 
-  int binarySum[numberOfBinaryDigits];
-  initializingOfArray(binarySum, numberOfBinaryDigits);
-  sumOfBinaryNumbers(binaryNumber1,  binaryNumber2, binarySum, numberOfBinaryDigits);
-
-  printf("First number in binary form : ");
+  printf("Greater number in binary form : ");
+  printSignOfNumber(signOfFirstNumber);
   printBinaryNumber(binaryNumber1, numberOfBinaryDigits);
-  printf("Second number in binary form : ");
+  printf("Another in binary form : ");
+  printSignOfNumber(signOfSecondNumber);
   printBinaryNumber(binaryNumber2, numberOfBinaryDigits);
   printf("The sum of the numbers in binary form : ");
+  printSignOfNumber(signOfSumOfNumbers);
   printBinaryNumber(binarySum, numberOfBinaryDigits);
 
   int decimalSum = 0;
   binaryToDecimal(&decimalSum, binarySum, numberOfBinaryDigits);
-  printf("The sum of the numbers in decimal form :  %d\n", decimalSum);
+  printf("The sum of the numbers in decimal form : ");
+  printSignOfNumber(signOfSumOfNumbers);
+  printf("%d\n", decimalSum);
   return 0;
 }
