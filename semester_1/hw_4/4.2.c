ce91464328a3c003035edd467d441aa056318b60
kate <shandkat3@gmail.com>
2019-12-22 16:57:15 +0300

    Удалил ненужные библиотеки

--- semester_1/hw_4/4.2.c
+++ semester_1/hw_4/4.2.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
 #include "phoneBook.h"
 
 int main()
 {
-  // printGreetings();
+  printGreetings();
 
   PhoneBook* phoneBook = getPhoneBook();
   commandProcessing(phoneBook);
